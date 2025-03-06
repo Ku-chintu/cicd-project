08146c41eef54d88393e67d856f67e734d4e422c
Ku-chintu <kumar.chintu1987@gmail.com>
2025-03-07 02:28:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@ resource "aws_instance" "name" {
     ami = "ami-0c50b6f7dc3701ddd"
     instance_type = "t2.micro"
     key_name = "Mumbai-key"
+ tags = {
+        Name  = "MyEC2Instance"
+        }
+
   }
